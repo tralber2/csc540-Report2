d4ace2d42ce0c71a7c4565bf7c0fa580009954fa
Tyler Albert <tralber2@ncsu.edu>
2018-04-12 17:33:03 -0400

    Maven added, api files added

---
+++ src/main/java/src/InfoProcUtils.java
@@ -0,0 +1,67 @@
+package src;
+
+public class InfoProcUtils {
+    public static void insertHotel() {
+
+    }
+
+    public static void updateHotel() {
+
+    }
+
+    public static void deleteHotel() {
+
+    }
+
+    public static void insertRoom() {
+
+    }
+
+    public static void updateRoom() {
+
+    }
+
+    public static void deleteRoom() {
+
+    }
+
+    public static void insertStaff() {
+
+    }
+
+    public static void updateStaff() {
+
+    }
+
+    public static void deleteStaff() {
+
+    }
+
+    public static void insertCustomer() {
+
+    }
+
+    public static void updateCustomer() {
+
+    }
+
+    public static void deleteCustomer() {
+
+    }
+
+    public static boolean checkRoomNumberAvailability() {
+        return false;
+    }
+
+    public static boolean checkRoomTypeAvailability() {
+        return false;
+    }
+
+    public static void assignRoomToCustomer() {
+
+    }
+
+    public static void releaseRoom() {
+
+    }
+}
